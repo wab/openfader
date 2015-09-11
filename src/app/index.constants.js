75d4ed66fd906a729c5651a40fd37a20c0c54755
wab <contact@wabdesign.fr>
2015-09-11 16:49:21 +0200

    init yeoman gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('openfader')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
