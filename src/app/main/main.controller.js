dc547928be7141c5e344de3877e6119983822ded
wab <contact@wabdesign.fr>
2015-09-11 17:34:22 +0200

    debut int wall with flex box

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,33 +7,54 @@
 
   /** @ngInject */
   function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+    var main = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1441982687804;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+    main.soirees = [
+      {
+        'id' : 0,
+        'slug' : 'part1',
+        'name' : 'openfader - part 1'
+      },
+      {
+        'id' : 1,
+        'slug' : 'part2',
+        'name' : 'openfader - part 2'
+      },
+      {
+        'id' : 2,
+        'slug' : 'part3',
+        'name' : 'openfader - part 3'
+      },
+      {
+        'id' : 3,
+        'slug' : 'part4',
+        'name' : 'openfader - part 4'
+      },
+      {
+        'id' : 4,
+        'slug' : 'pick',
+        'name' : 'openfader - Pick your djs'
+      },
+      {
+        'id' : 5,
+        'slug' : 'technorama',
+        'name' : 'openfader - technorama'
+      },
+      {
+        'id' : 6,
+        'slug' : 'boo',
+        'name' : 'openfader - booooo'
+      },
+      {
+        'id' : 7,
+        'slug' : 'pi',
+        'name' : 'openfader - le printemps industriel'
+      },
+      {
+        'id' : 8,
+        'slug' : 'close-up',
+        'name' : 'openfader - close up'
+      }
+    ];
   }
 })();
